e828d3b2871ef644110f09c58424a46f5e466d85
duckhee <fain9301@yahoo.com>
2019-10-17 15:51:59 +0900

    set Point02 explanation

--- sample-source/OPENCVBook/chapter03/3.1.1 point02/point2.cpp
+++ sample-source/OPENCVBook/chapter03/3.1.1 point02/point2.cpp
@@ -2,15 +2,21 @@
 
 int main()
 {
+    /* int형 Point 좌표 생성 */
     cv::Point_<int> pt1(100, 200);
+    /* float형 Point 좌표 생성 */
     cv::Point_<float> pt2(92.3f, 125.23f);
+    /* double형 Point 좌표 생성 */
     cv::Point_<double> pt3(100.2, 300.9);
-
+    /* int형 Point 좌표 간결한 생성 */
     cv::Point2i pt4(120, 69);
+    /* float형 Point 좌표 간결한 생성 */
     cv::Point2f pt5(0.3f, 0.f), pt6(0.f, 0.4f);
+    /* double형 Point 좌표 간결한 생성 */
     cv::Point2d pt7(0.25, 0.4f);
-
+    /* 자료형이 다른 Point 객체 덧셈 */
     cv::Point pt8 = pt1 + (cv::Point) pt2;
+    /* Point 객체에 대한 스칼라 곱 */
     cv::Point2f pt9 = pt6 * 3.14f;
     cv::Point2d pt10 = (pt3 + (cv::Point2d) pt6) * 10;
 
